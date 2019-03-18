a4762db21c7f6ed2c9049fd9b2a0b6b15bc49ac6
huanyu82 <1468963997@qq.com>
2019-03-18 21:19:21 +0800

    Update temp.py
    huanyu+1

--- temp.py
+++ temp.py
@@ -1,7 +1,13 @@
 out={'Python': 2, 'C++': 1, 'Java': 3, 'Go': 1, 'PHP': 1}
 for k,v in out.items():
         out[v]=k
+        print("huan4")
 print(out)
+<<<<<<< HEAD
+huanyu+1
 
+=======
+>>>>>>> abd03412d11ed9772fe2bc15a97f054715372ecc
 print("huanyu1")
-print（"zhu1"）
+print("zhu1")
+print("huan3")
